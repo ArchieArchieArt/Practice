532e07eed630a5f552640f2313d32ea4bc9dd123
a.magdiev <a.magdiev@eastwind.ru>
2025-02-17 11:25:41 +0500

    Remainder operator.

--- src/LessonDivisionFirstNumberOnSecond.java
+++ src/LessonDivisionFirstNumberOnSecond.java
@@ -1,23 +1,15 @@
 public class LessonDivisionFirstNumberOnSecond {
 
-/*    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        System.out.print("Input your numbers, please: ");
-        int[] inputOne = new int[256];
-        if(System.in.read(inputOne) == 0)
-            System.exit(0);
-        int[] inputTwo = new int[256];
-        if(System.in.read(inputTwo) == 0)
-            System.exit(0);
+        int inputOne = 14;
+        int inputTwo = 5;
 
-
-        if (inputOne||inputTwo.isEmpty()) {
-            System.out.println("There are no enough symbols");
-        } else if (inputTwo == 0) {
+        if (inputTwo == 0) {
             System.out.println("Cannot divide by zero");
         } else {
-            int thirdNumber = inputOne / inputTwo;
+            int thirdNumber = inputOne % inputTwo;
             System.out.println("Result of divide is - " + thirdNumber);
         }
-    }*/
+    }
 }
