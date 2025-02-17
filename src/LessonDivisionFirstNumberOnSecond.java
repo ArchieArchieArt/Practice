e348eab8ab183e2850d8840f2a3222958ee4121c
a.magdiev <a.magdiev@eastwind.ru>
2025-02-17 14:18:24 +0500

    New Type - Scanner.

--- src/LessonDivisionFirstNumberOnSecond.java
+++ src/LessonDivisionFirstNumberOnSecond.java
@@ -1,15 +1,37 @@
+import java.util.Scanner;
+
 public class LessonDivisionFirstNumberOnSecond {
 
     public static void main(String[] args) {
 
-        int inputOne = 14;
-        int inputTwo = 5;
+        System.out.print("Input your numbers, please: ");
+        Scanner newScanner = new Scanner(System.in);
 
-        if (inputTwo == 0) {
+        String stringOne = (newScanner.nextLine()).trim();
+        if (stringOne.isEmpty()) {
+            System.out.println("You have to enter dividend");
+            System.exit(0);
+        }
+
+        int numberOne = Integer.parseInt (stringOne);
+        System.out.println("Got number: '" + numberOne +"'");
+
+        System.out.print("Input your numbers, please: ");
+
+        String stringTwo = (newScanner.nextLine()).trim();
+        if (stringTwo.isEmpty()) {
+            System.out.println("You have to enter divisor");
+            System.exit(0);
+        }
+
+        int numberTwo = Integer.parseInt (stringTwo);
+        System.out.println("Got number: '" + numberTwo +"'");
+
+        if (numberTwo == 0) {
             System.out.println("Cannot divide by zero");
         } else {
-            int thirdNumber = inputOne % inputTwo;
-            System.out.println("Result of divide is - " + thirdNumber);
+            int numberThree = numberOne % numberTwo;
+            System.out.println("Result of divide is - " + numberThree);
         }
     }
 }
