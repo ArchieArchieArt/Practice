2be70f0faf6786a6e8fa8c884f82b7c283e55da0
a.magdiev <a.magdiev@eastwind.ru>
2025-02-18 11:13:38 +0500

    New Lesson of Conditions.

---
+++ src/LessonConditions.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LessonConditions {
+
+    public static void main(String[] args) {
+
+        System.out.print("Input number, please: ");
+        Scanner newScanner = new Scanner(System.in);
+
+        String stringOne = (newScanner.nextLine()).trim();
+        if (stringOne.isEmpty()) {
+            System.out.println("You have to enter number");
+            System.exit(0);
+        }
+
+        int number = Integer.parseInt (stringOne);
+        System.out.println("Got number: '" + number +"'");
+
+        if (((number >= -4) && (number <= 11)) || ((number >= 13) && (number <= 15)) || (number == 20)) {
+            System.out.println("Is TRUE");
+        } else {
+            System.out.println("Is FALSE");
+        }
+    }
+}
