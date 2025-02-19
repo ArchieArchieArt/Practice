51367bb2ea7ec0478a76c27d2722544b6e54a6dc
a.magdiev <a.magdiev@eastwind.ru>
2025-02-19 10:45:34 +0500

    New lessons.

--- src/LessonKbToByte.java
+++ src/LessonKbToByte.java
@@ -1,11 +1,15 @@
+/**
+ * Дано целое число, содержащее количество килобайт:
+ * int kb = 35;
+ * Переведите это значение в байты.
+ */
 public class LessonKbToByte {
 
     public static void main(String[] args) {
 
         int kb = 35;
         int bytes = kb * 1024;
-        byte b = (byte) bytes;
 
-        System.out.println(b);
+        System.out.println(bytes);
     }
 }
